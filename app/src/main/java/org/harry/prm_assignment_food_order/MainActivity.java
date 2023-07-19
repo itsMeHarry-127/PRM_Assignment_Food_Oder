c80c9dca67337ee0c0ffe90e6371142d12ff3335
itsMeHarry-127 <vietnh1207@gmail.com>
2023-07-19 11:12:25 +0700

    first commit

---
+++ app/src/main/java/org/harry/prm_assignment_food_order/MainActivity.java
@@ -0,0 +1,14 @@
+package org.harry.prm_assignment_food_order;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
